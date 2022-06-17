fc332045fd2e6a0939636de8baaeb094918e585c
Dev <dev3319gupta@gmial.com>
2022-06-17 20:23:21 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Board from "./components/Board"
+
+import './styles/root.scss' 
+
+const App = () => (
+  <div className="app">
+    <h1>TIC TAC TOE</h1>
+    <Board />
+  </div>
+);
+export default App ;
---
+++ src/components/Square.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Square =  ({  value}) =>{
+ 
+   return (
+    
+   <button type = "button" className='square' >
+    
+     { value }
+    
+    </button>
+    
+  )
+};
+
+export default Square
